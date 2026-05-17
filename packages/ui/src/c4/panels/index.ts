facf17434c8eb873ef31e4e64b3d49152155652c
RaghavChamadiya <raghavchamadiya@gmail.com>
2026-05-17 16:39:35 +0530

    feat(ui): add C4 level tabs, breadcrumb, legend, inspector panels

---
+++ packages/ui/src/c4/panels/index.ts
@@ -0,0 +1,7 @@
+export { C4LevelTabs } from "./C4LevelTabs";
+export type { C4LevelTabsProps } from "./C4LevelTabs";
+export { C4Breadcrumb } from "./C4Breadcrumb";
+export type { C4BreadcrumbProps } from "./C4Breadcrumb";
+export { C4Legend } from "./C4Legend";
+export { C4NodeInspector } from "./C4NodeInspector";
+export type { C4NodeInspectorProps } from "./C4NodeInspector";
