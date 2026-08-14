0dec39fc2da13658ef23831018f737f55c234cb2
mercael <mercael91@gmail.com>
2026-08-14 11:33:09 +0300

    Fix #1520: aliased imports should create graph edges

--- registry.ts
+++ registry.ts
@@ -1,2 +1,5 @@
 import { collateralEndpoint as collateralEndpoint } from "./collateral";
 import { pos as pos } from "./pos";
+// Use the aliased imports to create graph edges
+const collateral = collateralEndpoint;
+const posValue = pos;
